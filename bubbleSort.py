23cecf8a9a34bdf220799be26e808b4e1d54be28
Eric Lopez Morales <149924573+ELM31@users.noreply.github.com>
2025-02-24 00:16:41 -0800

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,9 +1,9 @@
 #bubblesort 
-def bubble_sort(students):
-    n = len (students)
+def bubble_sort(arr):
+    n = len (arr)
 
     for i in range(n):
         for j in range(0, n-i - 1):
-            if students[j][1] > students[j+1][1]:
-                students[j], students[j+1] = students[j+1], students[j]
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
